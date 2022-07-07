cf4b054b910490524e26270a89c04bd3e8b64430
JulienZaglia <jarjarbinkstobinks@gmail.com>
2022-07-07 07:46:22 +0200

    changement pour format php

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Julien Zaglia's Portfolio</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
-    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
-</head>
-
-<body>
-
-    <!-- loader -->
-    <div class="wrapper">
-        <div class="loader"></div>
-    </div>
-
-
-
-    <!-- header -->
-
-    <nav class="menu">
-        <div class="menu-left menu-item">
-            <div class="logo-1">Julien Zaglia.</div>
-            <div class="logo-2">Web developer</div>
-
-        </div>
-
-        <div class="menu-right menu-item">
-            <span class="menu-link"><a href="about.html"> About </a></span>
-            <span class="menu-link"><a href="lastestwork.html"> Lastest Work </a></span>
-            <span class="menu-link"><a href="contact.html"> Contact </a></span>
-        </div>
-
-    </nav>
-
+<?php 
+require_once ('init/header.php');
+?>
 
     <main>
 
@@ -95,3 +59,7 @@
 </body>
 
 </html>
+
+<?php 
+require_once ('init/footer.php');
+?>
---
+++ init/footer.php
@@ -0,0 +1,8 @@
+</main>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+<script src="index.js"></script>
+
+</body>
+
+</html>
